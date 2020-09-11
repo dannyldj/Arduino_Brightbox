bfbcf7d2f6a2e230bc0917b58c8b9fd6953e0770
Dongju Lee <dannyldj@naver.com>
2020-09-11 09:45:01 +0900

    Release 1.1
    code optimization

--- Controlbrightness.py
+++ Controlbrightness.py
@@ -1,119 +1,83 @@
+
 import serial
 import os
-import serial.tools.list_ports  # For listing available serial ports
+from serial.tools import list_ports  # For listing available serial ports
+import ctypes
 
-dev = serial.tools.list_ports.comports()
 
-ports = []
-
-for d in dev:
-    ports.append((d.device, d.serial_number))
-
+user = ctypes.windll.user32
+4
 print('Detected serial ports:')
-
-for d in ports:
-    print("Port:" + str(d[0]) + "\tSerial Number:" + str(d[1]))
+for d in serial.tools.list_ports.comports():
+    print("Port:" + str(d.device) + "\tSerial Number:" + str(d.serial_number))
 
 print('\n포트 번호를 입력주세요')
 port = input()
-ser = serial.Serial(port="COM"+port, baudrate=9600, )
+
+
+ser = serial.Serial(port="COM{}".format(port), baudrate=9600)
+#ser = serial.Serial(port="COM10", baudrate=9600)
 
 os.system(os.path.dirname(os.path.realpath(__file__)) + "\\First_Brightness.bat")
-
 cb = 100
+
+def change_brightness(curr_bn, to_bn):
+    if curr_bn > to_bn:
+        while curr_bn > to_bn:
+            os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
+            curr_bn -= 10
+    else:
+        while curr_bn < to_bn:
+            os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
+            curr_bn += 10
+    return curr_bn
+
 
 while True:
     if ser.readable():
         res = ser.readline()
-        print(res.decode()[:len(res) - 1])
-        if int(res.decode()[:len(res) - 1]) <= 70:
-            if cb < 100:
-                while cb != 100:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 70 < int(res.decode()[:len(res) - 1]) <= 120:
-            if cb > 90:
-                while cb > 90:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 90:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 120 < int(res.decode()[:len(res) - 1]) <= 170:
-            if cb > 80:
-                while cb > 80:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 80:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 170 < int(res.decode()[:len(res) - 1]) <= 220:
-            if cb > 70:
-                while cb > 70:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 70:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 220 < int(res.decode()[:len(res) - 1]) <= 270:
-            if cb > 60:
-                while cb > 60:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 60:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 270 < int(res.decode()[:len(res) - 1]) <= 320:
-            if cb > 50:
-                while cb > 50:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 50:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 320 < int(res.decode()[:len(res) - 1]) <= 370:
-            if cb > 40:
-                while cb > 40:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 40:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 370 < int(res.decode()[:len(res) - 1]) <= 420:
-            if cb > 30:
-                while cb > 30:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 30:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 420 < int(res.decode()[:len(res) - 1]) <= 470:
-            if cb > 20:
-                while cb > 20:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 20:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 470 < int(res.decode()[:len(res) - 1]) <= 520:
-            if cb > 10:
-                while cb > 10:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
-            else:
-                while cb < 10:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Increase_Brightness.bat")
-                    cb += 10
-        if 520 < int(res.decode()[:len(res) - 1]):
-            if cb > 0:
-                while cb > 0:
-                    os.system(os.path.dirname(os.path.realpath(__file__)) + "\\Decrease_Brightness.bat")
-                    cb -= 10
+
+        data = res.decode()[:-1]
+        print(data)
+
+        idx = data.index(':')
+        data_type = data[:idx]
+        data_value = data[idx + 1:]
+
+        if data_type == 'IR':
+            data_value = int(data_value)
+            print(data_value)
+            if data_value == 98:
+                # Windows + L
+                ctypes.windll.user32.LockWorkStation()
+        elif data_type == 'BR':
+            i_brightness = int(data_value)
+            # i_brightness = int(data_value)
+
+            print(i_brightness)
+            if i_brightness <= 70:  # ~ 70 100
+                cb = change_brightness(cb, 100)
+            elif 70 < i_brightness <= 120:  # 70 ~ 120 90
+                cb = change_brightness(cb, 90)
+            elif 120 < i_brightness <= 170:
+                cb = change_brightness(cb, 80)
+            elif 170 < i_brightness <= 220:
+                cb = change_brightness(cb, 70)
+            elif 220 < i_brightness <= 270:
+                cb = change_brightness(cb, 60)
+            elif 270 < i_brightness <= 320:
+                cb = change_brightness(cb, 50)
+            elif 320 < i_brightness <= 370:
+                cb = change_brightness(cb, 40)
+            elif 370 < i_brightness <= 420:
+                cb = change_brightness(cb, 30)
+            elif 420 < i_brightness <= 470:
+                cb = change_brightness(cb, 20)
+            elif 470 < i_brightness <= 520:
+                cb = change_brightness(cb, 10)
+            elif 520 < i_brightness:
+                cb = change_brightness(cb, 0)
+
+        else:
+            print('Err')
+
